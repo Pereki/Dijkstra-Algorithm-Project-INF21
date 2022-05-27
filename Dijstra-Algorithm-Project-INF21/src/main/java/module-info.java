478c5f8f7841f88ed1273ccc81b6be577bf329ce
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2022-05-27 16:23:34 +0200

    initial javafx hello world

---
+++ Dijstra-Algorithm-Project-INF21/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.dijstraalgorithmprojectinf21 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.dijstraalgorithmprojectinf21 to javafx.fxml;
+    exports com.example.dijstraalgorithmprojectinf21;
+}
