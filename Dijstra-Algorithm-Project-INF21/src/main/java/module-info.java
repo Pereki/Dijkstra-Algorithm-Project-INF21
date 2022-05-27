03954d633485138688046bc0d8f916d649910d75
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2022-05-27 16:25:48 +0200

    reset

--- Dijstra-Algorithm-Project-INF21/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.dijstraalgorithmprojectinf21 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.dijstraalgorithmprojectinf21 to javafx.fxml;
-    exports com.example.dijstraalgorithmprojectinf21;
-}
