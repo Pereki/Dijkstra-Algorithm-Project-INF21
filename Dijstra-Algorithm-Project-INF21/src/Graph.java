40c359e04f6dd02ae750f1a2eb9d6556fa94a015
Pereki <ruben.elias.kraft@outlook.de>
2022-06-01 09:09:59 +0200

    feat: add Dijkstra Algorithmn and major changes in Vertex, Graph and Edge class

--- Dijstra-Algorithm-Project-INF21/src/Graph.java
+++ Dijstra-Algorithm-Project-INF21/src/Graph.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Graph {
+    private ArrayList<Edge> Edgelist;
+
+    public Graph(ArrayList<Edge> edgesOfGraph){
+        this.Edgelist = edgesOfGraph;
+    }
+
+    public  ArrayList<Edge> getOptionsOfVertex(Vertex selectedVertex){
+
+        ArrayList<Edge> optionsEdges = new ArrayList<Edge>();
+        for (Edge edge: this.Edgelist) {
+            if(edge.getStartingVertex().getId() == selectedVertex.getId()){
+                optionsEdges.add(edge);
+            }
+        }
+        return optionsEdges;
+    }
+
 }
--- Dijstra-Algorithm-Project-INF21/src/Vertex.java
+++ Dijstra-Algorithm-Project-INF21/src/Vertex.java
@@ -2,13 +2,25 @@
 
 public class Vertex {
     private int id;
-
+    private int costs = 0;
+    private Vertex precessor;
     private double lat;//Breitengrad
     private double lon;//Längengrad
 
+    public Vertex(int id, double lat, double lon, Vertex precessor){
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+        this.precessor =  precessor;
+    }
     public Vertex(int id, double lat, double lon){
         this.id = id;
         this.lat = lat;
         this.lon = lon;
+        this.precessor = null;
+    }
+
+    public int getId(){
+        return id;
     }
 }
