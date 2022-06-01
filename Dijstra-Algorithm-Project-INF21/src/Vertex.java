fce2ec1bf18d925f6fe31a5e384870499ac7a9ed
PixelPasi <47497667+PixelPasi@users.noreply.github.com>
2022-06-01 17:21:16 +0200

    feat: add methods and attributes of Edge and Vertex

--- Dijstra-Algorithm-Project-INF21/src/Vertex.java
+++ Dijstra-Algorithm-Project-INF21/src/Vertex.java
@@ -6,21 +6,39 @@ public class Vertex {
     private Vertex precessor;
     private double lat;//Breitengrad
     private double lon;//Längengrad
+    private String identifier;//Bezeichner
+    private boolean junction;//anschlussstelle
 
-    public Vertex(int id, double lat, double lon, Vertex precessor){
+    public Vertex(int id, double lat, double lon, Vertex precessor, String identifier, boolean junction){
         this.id = id;
         this.lat = lat;
         this.lon = lon;
         this.precessor =  precessor;
+        this.identifier = identifier;
+        this.junction = junction;
     }
     public Vertex(int id, double lat, double lon){
         this.id = id;
         this.lat = lat;
         this.lon = lon;
         this.precessor = null;
+        this.identifier = null;
+        this.junction = false;
     }
 
     public int getId(){
         return id;
     }
+
+    public int getCosts(){return costs;}
+
+    public Vertex getPrecessor(){return precessor;}
+
+    public double getLat(){return lat;}
+
+    public double getLon(){return  lon;}
+
+    public String getIdentifier(){return identifier;}
+
+    public boolean getJunction(){return junction;}
 }
