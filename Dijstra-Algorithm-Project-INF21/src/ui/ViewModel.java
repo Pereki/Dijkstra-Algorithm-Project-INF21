66a46575b2a66bbd8689af266a60c132d6db2de7
dasdawidt <David_Maier@gmx.de>
2022-06-02 18:49:58 +0200

    feat: added ViewController class

---
+++ Dijstra-Algorithm-Project-INF21/src/ui/ViewModel.java
@@ -0,0 +1,20 @@
+package ui;
+
+import java.util.ArrayList;
+
+public class ViewModel {
+
+    private Object map;
+    private Object route;
+
+    private ArrayList<Object> listeners;
+
+    public void setMap(Object map) {
+
+    }
+
+    public void setRoute(Object route) {
+
+    }
+
+}
