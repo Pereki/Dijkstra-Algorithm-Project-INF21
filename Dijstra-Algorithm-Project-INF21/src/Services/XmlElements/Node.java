f01cd9aefde39c013c4f1c34563c07ba0854185e
NoName11234 <47484268+NoName11234@users.noreply.github.com>
2022-06-03 08:41:30 +0200

    feat: finished node class

--- Dijstra-Algorithm-Project-INF21/src/Services/XmlElements/Node.java
+++ Dijstra-Algorithm-Project-INF21/src/Services/XmlElements/Node.java
@@ -1,4 +1,31 @@
 package Services.XmlElements;
 
 public class Node {
+    private int id;
+    private int lat;
+    private int lon;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(int lon) {
+        this.lon = lon;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public int getLon() {
+        return lon;
+    }
 }
