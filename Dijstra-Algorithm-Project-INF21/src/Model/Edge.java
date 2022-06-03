05d58072f66666aa5fa4b82ffb7e70a863aa37bf
PixelPasi <47497667+PixelPasi@users.noreply.github.com>
2022-06-03 10:51:08 +0200

    fix:change Variables name to lowercase + List to Arraylist

--- Dijstra-Algorithm-Project-INF21/src/Model/Edge.java
+++ Dijstra-Algorithm-Project-INF21/src/Model/Edge.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Edge implements Serializable {
     private Vertex v1;
@@ -28,4 +29,17 @@ public class Edge implements Serializable {
     public int getMarking(){return marking;}
 
     public double getLength(){return length;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Edge)) return false;
+        Edge edge = (Edge) o;
+        return getMarking() == edge.getMarking() && Double.compare(edge.getLength(), getLength()) == 0 && Objects.equals(v1, edge.v1) && Objects.equals(v2, edge.v2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(v1, v2, getMarking(), getLength());
+    }
 }
