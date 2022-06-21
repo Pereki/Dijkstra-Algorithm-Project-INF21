a6c9076b394246e19aaf209aa4f9505a23caf077
dasdawidt <David_Maier@gmx.de>
2022-06-21 03:01:01 +0200

    feat: added svg background map and zoom buttons

--- Dijkstra-Algorithm-Project-INF21/src/main/java/controller/Controller.java
+++ Dijkstra-Algorithm-Project-INF21/src/main/java/controller/Controller.java
@@ -5,23 +5,33 @@ import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.SVGPath;
 import model.Edge;
 import model.Graph;
 import model.Vertex;
+import service.SVGParser;
 import view.GraphRenderer;
 
+import java.io.File;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
+    private ScrollPane scrollpane;
+    @FXML
     private StackPane pane;
     @FXML
-    private Group group;
+    private Group groupGraphs;
+    @FXML
+    private Group groupBackground;
 
     @FXML
     private TextField field1;
@@ -36,7 +46,21 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        this.renderer = new GraphRenderer(group, pane);
+        this.renderer = new GraphRenderer(groupGraphs, pane);
+
+        File f = new File(
+                "C:\\Users\\David\\OneDrive\\Dokumente\\Beruflich\\Duales Studium\\DH\\Vorlesungen\\2. Semester\\Programmieren\\Programmierprojekt\\Sonstige\\de.svg"
+        );
+        List<List<SVGPath>> svgs = null;
+        try {
+            svgs = SVGParser.parse(f);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        for (SVGPath p : svgs.get(0)) {
+            p.setFill(Paint.valueOf("#bfbfbf"));
+            this.groupBackground.getChildren().add(p);
+        }
     }
 
     @FXML
@@ -70,7 +94,24 @@ public class Controller implements Initializable {
     }
 
     @FXML
-    protected void onClearButtonClick() {
+    protected void onButtonZoomOutClick() {
+        double scale = pane.getScaleX();
+        scale -= 0.1;
+        if (scale < 0.1) return;
+        setZoomFactor(scale);
+    }
 
+    @FXML
+    protected void onButtonZoomInClick() {
+        double scale = pane.getScaleX();
+        scale += 0.1;
+        if (scale > 2) return;
+        setZoomFactor(scale);
+    }
+
+    protected void setZoomFactor(double factor) {
+        pane.setScaleX(factor);
+        pane.setScaleY(factor);
+        pane.setScaleY(factor);
     }
 }
