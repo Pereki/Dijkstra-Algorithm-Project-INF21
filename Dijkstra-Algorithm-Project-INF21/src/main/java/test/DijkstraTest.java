6b7540044469f21a8cf95ba35b82e45bb4c9e250
L4kiLuk <49391930+L4kiLuk@users.noreply.github.com>
2022-06-27 13:51:50 +0200

    feat: add calcDistance() in Edge to calc the distance between to coordinates. feat: add Edge(V1,V2) in Edge a constructor without length with internal calculation of it.

--- Dijkstra-Algorithm-Project-INF21/src/main/java/test/DijkstraTest.java
+++ Dijkstra-Algorithm-Project-INF21/src/main/java/test/DijkstraTest.java
@@ -13,8 +13,8 @@ import java.util.List;
 
 public class DijkstraTest {
     public static void main(String[] args) throws Exception {
-        Vertex A = new Vertex(1,34.4,57.8);
-        Vertex B = new Vertex(2,34.5,57.8);
+        Vertex A = new Vertex(1,48.44534,8.69034);
+        Vertex B = new Vertex(2,48.44274,8.69067);
         Vertex C = new Vertex(3,34.6,57.8);
         Vertex D = new Vertex(4,34.7,57.8);
         Vertex E = new Vertex(5,34.8,57.8);
@@ -26,21 +26,21 @@ public class DijkstraTest {
 
         ArrayList vertexes = new ArrayList(Arrays.asList(new Vertex[]{A,B,C,D,E,F,G,H,I}));
 
-        Edge e1 = new Edge(A,B,2);
-        Edge e2 = new Edge(B,C,4);
-        Edge e3 = new Edge(C,D,2);
-        Edge e4 = new Edge(D,E,1);
-        Edge e5 = new Edge(E,F,6);
-        Edge e6 = new Edge(F,A,9);
-        Edge e7 = new Edge(A,G,15);
-        Edge e8 = new Edge(B,G,6);
-        Edge e9 = new Edge(C,I,15);
-        Edge e10 = new Edge(D,I,1);
-        Edge e11 = new Edge(E,H,3);
-        Edge e12 = new Edge(F,H,11);
-        Edge e13 = new Edge(G,I,2);
-        Edge e14 = new Edge(I,H,4);
-        Edge e15 = new Edge(H,G,15);
+        Edge e1 = new Edge(A,B);
+        Edge e2 = new Edge(B,C);
+        Edge e3 = new Edge(C,D);
+        Edge e4 = new Edge(D,E);
+        Edge e5 = new Edge(E,F);
+        Edge e6 = new Edge(F,A);
+        Edge e7 = new Edge(A,G);
+        Edge e8 = new Edge(B,G);
+        Edge e9 = new Edge(C,I);
+        Edge e10 = new Edge(D,I);
+        Edge e11 = new Edge(E,H);
+        Edge e12 = new Edge(F,H);
+        Edge e13 = new Edge(G,I);
+        Edge e14 = new Edge(I,H);
+        Edge e15 = new Edge(H,G);
         ArrayList edges = new ArrayList(Arrays.asList(new Edge[]{e1,e2,e3,e4,e5,e6,e7,e8,e9,e10,e11,e12,e13,e14,e15}));
 
         Graph raw = new Graph(edges,vertexes);
