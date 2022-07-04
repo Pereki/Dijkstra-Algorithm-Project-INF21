446d1f5cfccfa17aa9a84ac41a1627d5e9cd19fa
dasdawidt <David_Maier@gmx.de>
2022-07-04 16:39:09 +0200

    feat: simplified MercatorProjector and added unit tests for it

--- Dijkstra-Algorithm-Project-INF21/src/main/java/service/MercatorProjector.java
+++ Dijkstra-Algorithm-Project-INF21/src/main/java/service/MercatorProjector.java
@@ -5,47 +5,48 @@ import model.CoordinatePair;
 public class MercatorProjector {
     private CoordinatePair center;
 
-    public MercatorProjector(CoordinatePair center) {
-        this.center = center;
-    }
-
+    /**
+     * Creates a MercatorProjector with the given geo coordinates as center.
+     * @param latitude The latitude in degrees
+     * @param longitude The longitude in degrees
+     */
     public MercatorProjector(double latitude, double longitude) {
         this.center = new CoordinatePair(latitude, longitude);
     }
 
-    public CoordinatePair project(CoordinatePair coordinates) {
-        return project(coordinates.getLatitude(), coordinates.getLongitude());
+    /**
+     * Projects the given longitude to an x-coordinate in the Mercator-system.
+     * @param longitude The longitude in degrees
+     * @return The x-coordinate in radians
+     */
+    public double getX(double longitude) {
+        return Math.toRadians(longitude - center.getLongitude());
     }
 
-    public CoordinatePair project(double latitude, double longitude) {
-        return new CoordinatePair(getX(latitude), getY(longitude));
+    /**
+     * Projects the given latitude to a y-coordinate in the Mercator-system.
+     * @param latitude The latitude in degrees
+     * @return The y-coordinate in radians
+     */
+    public double getY(double latitude) {
+        return Math.log(Math.tan(Math.PI/4 + Math.toRadians(latitude)/2));
     }
 
-    public CoordinatePair unproject(double x, double y) {
-        return new CoordinatePair(getLongitude(x), getLatitude(y));
+    /**
+     * Projects the given x-coordinate to the longitude in the Mercator-system.
+     * @param x The x-coordinate in radians
+     * @return The longitude in degrees
+     */
+    public double getLongitude(double x) {
+        return Math.toDegrees(x) + center.getLongitude();
     }
 
-    public static CoordinatePair project(CoordinatePair coordinates, CoordinatePair center) {
-        return project(coordinates.getLatitude(), coordinates.getLongitude(), center);
-    }
-
-    public static CoordinatePair project(double latitude, double longitude, CoordinatePair center) {
-        return new MercatorProjector(center).project(latitude, longitude);
-    }
-
-    private double getX(double longitude) {
-        return longitude - center.getLongitude();
-    }
-
-    private double getY(double latitude) {
-        return Math.log(Math.tan(Math.PI/4 + latitude/2));
-    }
-
-    private double getLongitude(double x) {
-        return x + center.getLongitude();
-    }
-
-    private double getLatitude(double y) {
-        return Math.asin(Math.tanh(y));
+    /**
+     * Projects the given y-coordinate to the latitude in the Mercator-system.
+     * @param y The y-coordinate in radians
+     * @return The latitude in degrees
+     */
+    public double getLatitude(double y) {
+        return Math.toDegrees(Math.asin(Math.tanh(y)));
     }
 }
