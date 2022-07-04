085280f142b2e0a000a51045bcbfd4e75c736282
Pereki <ruben.elias.kraft@outlook.de>
2022-07-04 17:48:35 +0200

    feat: change to vertex instead of array

--- Dijkstra-Algorithm-Project-INF21/src/main/java/service/OpenMapRequester.java
+++ Dijkstra-Algorithm-Project-INF21/src/main/java/service/OpenMapRequester.java
@@ -1,4 +1,6 @@
-package Services;
+package service;
+
+import model.*;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,13 +14,23 @@ import java.net.URLConnection;
  * @author i21015
  */
 public class OpenMapRequester {
+    public static void main(String[] args) throws IOException {
+        Vertex v = new Vertex(0, 49.41461, 8.681495 );
+        Vertex b = new Vertex(1,49.420318, 8.687872);
+        OpenMapRequester o = new OpenMapRequester();
+
+        System.out.println(o.getDistance(v,b)+ "\n");
+        System.out.println(o.getDuration(v,b) + "\n");
+        System.out.println(o.getCompleteReturn(v,b)+ "\n");
+
+    }
     /**
      * Returns the duration of the path in seconds.
-     * @param array of the starting coordinates in pattern: east coordinates, north coordinates.
-     * @param array of the ending coordinates in Pattern: East Coordinates, North Coordinates.
+     * @param start start of the starting coordinates in pattern: east coordinates, North/South coordinates.
+     * @param end end of the ending coordinates in Pattern: East/West Coordinates, North/South Coordinates.
      * @return a double with the duration in seconds.
      */
-    public double getDuration(String[] start, String[] end) throws IOException {
+    public double getDuration(Vertex start, Vertex end) throws IOException {
         String result = getCompleteReturn(start, end);
 
         String[] resultsplit = result.split("\"summary\":");
@@ -27,8 +39,13 @@ public class OpenMapRequester {
         return Double.parseDouble(returnsplited[1]);
     }
 
-
-    public double getDistance(String[] start, String[] end) throws IOException {
+    /**
+     * Returns the distance of the path in seconds.
+     * @param start start of the starting coordinates in pattern: east coordinates, North/South coordinates.
+     * @param end end of the ending coordinates in Pattern: East/West Coordinates, North/South Coordinates.
+     * @return a double with the distance in meters.
+     */
+    public double getDistance(Vertex start, Vertex end) throws IOException {
         String result = getCompleteReturn(start, end);
 
         String[] resultsplit = result.split("\"summary\":");
@@ -37,9 +54,14 @@ public class OpenMapRequester {
         return Double.parseDouble(returner[1]);
     }
 
-
-    public String getCompleteReturn(String[] start, String[] end) throws IOException {
-        String url = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248595d0baf4b204b90ad00de85ee212c7f&start=" + start[0] + "," + start[1] + "&end=" + end[0] + "," + end[1];
+    /**
+     * Returns the complete request.
+     * @param start start of the starting coordinates in pattern: east coordinates, North/South coordinates.
+     * @param end end of the ending coordinates in Pattern: East/West Coordinates, North/South Coordinates.
+     * @return a double with the distance in meters.
+     */
+    public String getCompleteReturn(Vertex start, Vertex end) throws IOException {
+        String url = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248595d0baf4b204b90ad00de85ee212c7f&start=" + start.getLon() + "," + start.getLat() + "&end=" + end.getLon() + "," + end.getLat();
         URL apicall = new URL(url);
         URLConnection connection = apicall.openConnection();
 
@@ -53,7 +75,9 @@ public class OpenMapRequester {
             returner += inputLine;
         }
 
+
         in.close();
         return returner;
-        }
+    }
+
 }
